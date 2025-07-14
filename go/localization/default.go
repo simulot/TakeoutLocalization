0e4a11180fb57363437504800c92fc42f36e8c81
Simulot <jfcassan+github@gmail.com>
2025-07-14 16:32:42 +0200

    Refactor GetDefaultLocalizations to use sync.Once for lazy initialization

--- go/localization/default.go
+++ go/localization/default.go
@@ -2,25 +2,33 @@ package localization
 
 import (
 	"io/fs"
+	"sync"
 
 	"github.com/simulot/TakeoutLocalization/localizations"
 )
 
+var (
+	_products     Products
+	_onceProducts sync.Once
+)
+
 // GetDefaultLocalizations returns the default localizations embedded into this package.
 func GetDefaultLocalizations() Products {
-	vfs := localizations.Default
+	_onceProducts.Do(func() {
+		vfs := localizations.Default
 
-	entries, err := fs.ReadDir(vfs, ".")
-	if err != nil {
-		panic(err)
-	}
-	files := make([]string, len(entries))
-	for i, entry := range entries {
-		files[i] = entry.Name()
-	}
-	p, err := LoadLocalizations(vfs, files...)
-	if err != nil {
-		panic(err)
-	}
-	return p
+		entries, err := fs.ReadDir(vfs, ".")
+		if err != nil {
+			panic(err)
+		}
+		files := make([]string, len(entries))
+		for i, entry := range entries {
+			files[i] = entry.Name()
+		}
+		_products, err = LoadLocalizations(vfs, files...)
+		if err != nil {
+			panic(err)
+		}
+	})
+	return _products
 }
